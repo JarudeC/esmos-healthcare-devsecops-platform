ac1c0c491af4acb6b84c668ffa11aed27c5d0a89
JarudeC <jarude.xy.chan@gmail.com>
2026-03-16 20:51:56 +0800

    Fix ArgoCD OCI config: use values block instead of set for comma-separated list

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -48,10 +48,13 @@ resource "helm_release" "argocd" {
   }
 
   # Enable OCI Helm repo support (required for Bitnami charts)
-  set {
-    name  = "configs.cm.helm\\.valuesFileSchemes"
-    value = "secrets+gpg-import,secrets+gpg-import-kubernetes,secrets+age-import,secrets+age-import-kubernetes,secrets,secrets+literal,https,http,oci"
-  }
+  values = [<<-EOT
+    configs:
+      cm:
+        helm.valuesFileSchemes: >-
+          secrets+gpg-import,secrets+gpg-import-kubernetes,secrets+age-import,secrets+age-import-kubernetes,secrets,secrets+literal,https,http,oci
+  EOT
+  ]
 
   # Resource limits for e2-medium nodes
   set {
