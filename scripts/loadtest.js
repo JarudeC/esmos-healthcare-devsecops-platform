f9b52a5a1846e17b3f430dcd70e4bdf322d3f3fa
Wei Rong <developer.swr.2001@gmail.com>
2026-03-31 22:25:21 +0800

    update HPA

--- scripts/loadtest.js
+++ scripts/loadtest.js
@@ -3,16 +3,14 @@ import { sleep, check } from 'k6';
 
 export const options = {
   vus: 500,
-  duration: '10m',
+  duration: '5m',
   noConnectionReuse: true,
 };
 
 export default function () {
   const resOne = http.get('http://127.0.0.1:8888');
   const resTwo = http.get('http://127.0.0.1:8888/login/index.php');
-  const resThree = http.get('http://127.0.0.1:8888/course/');
 
   check(resOne, { 'home 200': (r) => r.status === 200 });
   check(resTwo, { 'login 200': (r) => r.status === 200 });
-  check(resThree, { 'course 200': (r) => r.status === 200 });
 }
